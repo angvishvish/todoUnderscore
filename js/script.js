ee5765c396ff9bd550540f94171cd8a27092c83f
angvishvish <angvish@promobitech.com>
2016-03-04 18:31:27 +0530

    setting up the project

---
+++ js/script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+(function (window, $) {
+  var todoTaskClass = function () {
+    const enterKey = 13;
+    var _todoScript,
+        _todoArray,
+        _newTaskText,
+        _clearAll;
+    
+    var _init = function () {
+      _todoScript   = $('#todo-ul-script');
+      _newTaskText  = $('#new-task');
+      _clearAll     = $('.clear-all');
+      _todoArray    = taskApi;
+
+      _setUpDom();
+      _bindUIActions();
+    };
+
+    function _updateDom () {
+
+    }
+    function _bindUIActions () {
+      $(_newTaskText).keypress(function(event) {
+        if(event.charCode == enterKey) {
+          _addTask();
+        }
+      });
+      
+      $(_clearAll).click(function (event) {
+        event.preventDefault();
+        _emptyArray();
+      });
+    }
+    
+    function _setUpDom () {
+      var todoTemplate    = _.template(_todoScript.html()),
+          todoApi         = todoTemplate({todoArray: _todoArray });
+
+      // Update the dom
+      $('.todo-ul-list').html(todoApi);
+    }
+
+    function _nextId () {
+      return _todoArray.length + 1;
+    }
+    
+    function _checkInput () {
+      return _newTaskText.text();
+    }
+
+    function _emptyInput () {
+      _newTaskText.val('');
+    }
+
+    function _addTask () {
+      var _unique = _nextId();
+
+      if (!_checkInput()) {
+        _todoArray.push({
+          "tags": [],
+          "position": 0,
+          "status": {
+            "displayString":"incomplete",
+            "name":"INCOMPLETE",
+          },
+          "root":false,
+          "id":   _unique,
+          "name": _newTaskText.val(),
+          "comments": [],
+          "completed": false
+        });
+
+        _updateDom();
+        _emptyInput();
+        _setUpDom();
+      }
+    }
+
+    function _emptyArray () {
+      _todoArray = [];
+      _setUpDom();
+    }
+    return {
+      init      : _init
+    }
+  }
+  // console.log(todoTaskClass)
+  var todo = new todoTaskClass();
+  todo.init();
+
+})(window, jQuery);
---
+++ js/taskApi.js
@@ -0,0 +1,54 @@
+var taskApi = [
+  {
+    "tags": [],
+    "position": 0,
+    "status": {
+      "displayString":"incomplete",
+      "name":"INCOMPLETE",
+    },
+    "root":false,
+    "id": 1,
+    "name": "Fix Electronics",
+    "comments":[],
+    "completed":false
+  },
+  {
+    "tags": [],
+    "position": 0,
+    "status": {
+      "displayString":"incomplete",
+      "name":"INCOMPLETE",
+    },
+    "root":false,
+    "id": 2,
+    "name": "Repair Table",
+    "comments":[],
+    "completed":false
+  },
+  {
+    "tags": [],
+    "position": 0,
+    "status": {
+      "displayString":"incomplete",
+      "name":"INCOMPLETE",
+    },
+    "root":false,
+    "id": 3,
+    "name": "Watch Movie",
+    "comments":[],
+    "completed":false
+  },
+  {
+    "tags": [],
+    "position": 0,
+    "status": {
+      "displayString":"incomplete",
+      "name":"INCOMPLETE",
+    },
+    "root":false,
+    "id": 4,
+    "name": "Go to home",
+    "comments":[],
+    "completed":false
+  }
+]
